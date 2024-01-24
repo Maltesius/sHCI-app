67d6654e8c69bf7471ed52196d5e2af7e891f411
Maltesius <andersmalta@gmail.com>
2024-01-24 01:40:14 +0100

    Added const for performance Formatted code

--- lib/views/leaderboards/global_leaderboard.dart
+++ lib/views/leaderboards/global_leaderboard.dart
@@ -64,65 +64,90 @@ class GlobalLeaderboard extends StatelessWidget {
                       bool isFirstPlace = index == 1;
                       bool isSecondPlace = index == 2;
                       bool isThirdPlace = index == 3;
-                      final orderTile = Container(decoration: isFirstPlace ? const BoxDecoration(
-                        // Create a gradient background
-                        gradient: LinearGradient(
-                          begin: Alignment.topRight,
-                          end: Alignment.bottomLeft,
-                          colors: [Colors.amber, Colors.white70, Colors.amber, Colors.amberAccent],
-                        ),
-                      ) : isSecondPlace ? const BoxDecoration(
-                        // Create a gradient background
-                        gradient: LinearGradient(
-                          begin: Alignment.topRight,
-                          end: Alignment.bottomLeft,
-                          colors: [Colors.grey, Colors.white54, Colors.grey, Colors.grey],
-                        ),
-                      ) : isThirdPlace ? const BoxDecoration(
-                        // Create a gradient background
-                        gradient: LinearGradient(
-                          begin: Alignment.topRight,
-                          end: Alignment.bottomLeft,
-                          colors: [Colors.brown, Colors.white38, Color.fromARGB(1, 136, 106, 80), Colors.brown],
-                        ),
-                      ) : const BoxDecoration(
-                        // Create a gradient background
-                        gradient: LinearGradient(
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
-                          colors: [Colors.white,Colors.white],
-                        ),
-                      ),child:ListTile(
-                        leading: Image(
-                            image: AssetImage(
-                                'assets/drinkImages/$drink.png')),
-                        title: Text(drink,
-                            style: const TextStyle(fontFamily: 'Noto', fontWeight: FontWeight.bold)),
-                        trailing: isFirstPlace
-                            ? Icon(Icons.looks_one_rounded)
+                      final orderTile = Container(
+                        decoration: isFirstPlace
+                            ? const BoxDecoration(
+                                // Create a gradient background
+                                gradient: LinearGradient(
+                                  begin: Alignment.topRight,
+                                  end: Alignment.bottomLeft,
+                                  colors: [
+                                    Colors.amber,
+                                    Colors.white70,
+                                    Colors.amber,
+                                    Colors.amberAccent
+                                  ],
+                                ),
+                              )
                             : isSecondPlace
-                            ? Icon(Icons.looks_two_rounded)
-                            : isThirdPlace
-                            ? Icon(IconData(0xf88c,
-                            fontFamily: 'MaterialIcons'))
-                            : Padding(
-                          padding:
-                          EdgeInsets.fromLTRB(0, 0, 9, 0),
-                          child: Text('$index',
+                                ? const BoxDecoration(
+                                    // Create a gradient background
+                                    gradient: LinearGradient(
+                                      begin: Alignment.topRight,
+                                      end: Alignment.bottomLeft,
+                                      colors: [
+                                        Colors.grey,
+                                        Colors.white54,
+                                        Colors.grey,
+                                        Colors.grey
+                                      ],
+                                    ),
+                                  )
+                                : isThirdPlace
+                                    ? const BoxDecoration(
+                                        // Create a gradient background
+                                        gradient: LinearGradient(
+                                          begin: Alignment.topRight,
+                                          end: Alignment.bottomLeft,
+                                          colors: [
+                                            Colors.brown,
+                                            Colors.white38,
+                                            Color.fromARGB(1, 136, 106, 80),
+                                            Colors.brown
+                                          ],
+                                        ),
+                                      )
+                                    : const BoxDecoration(
+                                        // Create a gradient background
+                                        gradient: LinearGradient(
+                                          begin: Alignment.topLeft,
+                                          end: Alignment.bottomRight,
+                                          colors: [Colors.white, Colors.white],
+                                        ),
+                                      ),
+                        child: ListTile(
+                          leading: Image(
+                              image:
+                                  AssetImage('assets/drinkImages/$drink.png')),
+                          title: Text(drink,
                               style: const TextStyle(
                                   fontFamily: 'Noto',
-                                  fontWeight:
-                                  FontWeight.w700)),
+                                  fontWeight: FontWeight.bold)),
+                          trailing: isFirstPlace
+                              ? const Icon(Icons.looks_one_rounded)
+                              : isSecondPlace
+                                  ? const Icon(Icons.looks_two_rounded)
+                                  : isThirdPlace
+                                      ? const Icon(IconData(0xf88c,
+                                          fontFamily: 'MaterialIcons'))
+                                      : Padding(
+                                          padding: const EdgeInsets.fromLTRB(
+                                              0, 0, 9, 0),
+                                          child: Text('$index',
+                                              style: const TextStyle(
+                                                  fontFamily: 'Noto',
+                                                  fontWeight: FontWeight.w700)),
+                                        ),
+                          minVerticalPadding: 22,
+                          tileColor: isFirstPlace
+                              ? Colors.amber
+                              : isSecondPlace
+                                  ? Colors.grey
+                                  : isThirdPlace
+                                      ? Colors.brown
+                                      : Colors.white,
                         ),
-                        minVerticalPadding: 22,
-                        tileColor: isFirstPlace
-                            ? Colors.amber
-                            : isSecondPlace
-                            ? Colors.grey
-                            : isThirdPlace
-                            ? Colors.brown
-                            : Colors.white,
-                      ),);
+                      );
                       tilesList.add(orderTile);
                     });
                     return Expanded(
@@ -136,6 +161,7 @@ class GlobalLeaderboard extends StatelessWidget {
                         heightFactor: 100,
                         widthFactor: 100,
                         child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             CircularProgressIndicator(),
                             Padding(
@@ -146,7 +172,6 @@ class GlobalLeaderboard extends StatelessWidget {
                               ),
                             )
                           ],
-                          mainAxisAlignment: MainAxisAlignment.center,
                         ),
                       ),
                     );
